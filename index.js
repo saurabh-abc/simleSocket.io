6c7a5deeb71fa0ef0752546b6af60d0ca95f14ed
saurabh-abc <58029228+saurabh-abc@users.noreply.github.com>
2023-10-10 04:28:49 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const app = express();
+const {Server} = require('socket.io')
+const server = http.createServer(app);
+const io = new Server(server);
+io.on('connection',(socket)=>{
+    socket.on('user-message',(message)=>{
+        io.emit('message',message);
+    })
+})
+app.use(express.static(path.resolve("./public")));
+app.get('/',(req,res)=>{
+    return res.sendFile('./public/index.html');
+})
+server.listen(3000,()=>{
+    console.log('server is run on 3000');
+})
